6411f04544a944d9e519804c14fcdf67c7d0b770
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-10 06:35:44 -0600

    return a string array for recursion?

--- 20200202-binaryTreeSerialize/src/Main.java
+++ 20200202-binaryTreeSerialize/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Node node = new Node("root",
@@ -10,6 +12,7 @@ public class Main {
         Workload w = new Workload();
         String serialized = w.serialize(node);
         System.out.println("serialized: " + serialized);
+        // serialized: root left left.left null null  null  right null null
         // serialized: root left left.left right
 
 //        String test = w.deserialize(serialized).left.left.val;
@@ -19,28 +22,44 @@ public class Main {
 
 class Workload {
     public String serialize(Node n) {
-        String serld = "";
+        String serld = "null";
         if (null != n) {
             serld = String.valueOf(n.val) + " "
 //                + String.valueOf(n.left.val)
 //                + String.valueOf(n.right.val);
-                    + serialize(n.left)
-                    + serialize(n.right);
+                    + serialize(n.left) + " "
+                    + serialize(n.right) + " ";
         }
 
         return serld;
     }
 
     public Node deserialize(String s) {
-        // root left left.left right
-        return new Node("0");
-    }
+        // root left left.left null null  null  right null null
+        String[] parts = s.split(" ");
+
+        if (parts[0].equalsIgnoreCase("null")) {
+            return new Node();
+        }
+
+        Node n = new Node(parts[0],
+                deserialize(String.join(" ", parts)),
+                deserialize(String.join(" ", parts))
+        );
+        return new Node();
+    } // return a joined string array of next stuff?
 }
 
 class Node {
     public Node left;
     public Node right;
     public String val;
+
+    public Node() {
+        this.left=null;
+        this.right=null;
+        this.val = "null";
+    }
 
     public Node(String val) {
         this.left=null;
