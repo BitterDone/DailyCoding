0da2e3e4a8804a66791eaf1867237b3be88d8f44
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-10 05:37:17 -0600

    add challenge and serialize

--- 20200202-binaryTreeSerialize/src/Main.java
+++ 20200202-binaryTreeSerialize/src/Main.java
@@ -1,20 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {10, 15, 3, 7, 18, -1, -32};
+        Node node = new Node("root",
+                new Node("left",
+                        new Node("left.left"),
+                        null),
+                new Node ("right"));
+        // node = Node('root', Node('left', Node('left.left')), Node('right'))
 
+        Workload w = new Workload();
+        String serialized = w.serialize(node);
+        System.out.println("serialized: " + serialized);
+        // serialized: root left left.left right
+
+//        String test = w.deserialize(serialized).left.left.val;
+//        System.out.println("Expect true: " + test.equalsIgnoreCase("left.left"));
     }
 }
 
 class Workload {
+    public String serialize(Node n) {
+        String serld = "";
+        if (null != n) {
+            serld = String.valueOf(n.val) + " "
+//                + String.valueOf(n.left.val)
+//                + String.valueOf(n.right.val);
+                    + serialize(n.left)
+                    + serialize(n.right);
+        }
 
+        return serld;
+    }
+
+    public Node deserialize(String s) {
+        // root left left.left right
+        return new Node("0");
+    }
 }
 
 class Node {
-    private Node left;
-    private Node right;
-    private int val;
+    public Node left;
+    public Node right;
+    public String val;
 
-    public Node(int val, Node left, Node right) {
+    public Node(String val) {
+        this.left=null;
+        this.right=null;
+        this.val = val;
+    }
+
+    public Node(String val, Node left, Node right) {
         this.left=left;
         this.right=right;
         this.val = val;
