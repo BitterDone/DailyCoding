9dd599ddcac9686f1be1592b9d7e0dc9b05e152c
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-15 11:02:40 -0600

    Intellij project

---
+++ 20200204-consCar/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Workload w = new Workload();
+        System.out.println(w.newMethod() + "\n");
+    }
+}
+
+class Workload {
+    public void newMethod() {
+    }
+}
