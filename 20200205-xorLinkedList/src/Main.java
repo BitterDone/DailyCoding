6cf7ec6b4d68077565cada56c60164aed1cc686b
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-15 21:06:43 -0600

    pasted email in

--- 20200205-xorLinkedList/src/Main.java
+++ 20200205-xorLinkedList/src/Main.java
@@ -1,30 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Workload w = new Workload();
-        int three = w.car(w.cons(3, 4));
-        int four = w.cdr(w.cons(3, 4));
-        System.out.println("3: " + three + "\n");
-        System.out.println("4: " + four + "\n");
+        System.out.println("3: " + "\n");
     }
 }
 
 class Workload {
-    public Pair cons(int f, int l) {
-        return new Pair(f, l);
-    }
-    public int car(Pair p) {
-        return p.first;
-    }
-    public int cdr(Pair p) {
-        return p.last;
-    }
-}
-
-class Pair {
-    int first;
-    int last;
-    public Pair(int f, int l) {
-        this.first = f;
-        this.last = l;
-    }
 }
