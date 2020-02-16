d14edfeb6c91d1e43026f47c50470892ff60413c
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-15 20:10:07 -0600

    Works

--- 20200204-consCar/src/Main.java
+++ 20200204-consCar/src/Main.java
@@ -1,11 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         Workload w = new Workload();
-        System.out.println(w.newMethod() + "\n");
+        int three = w.car(w.cons(3, 4));
+        int four = w.cdr(w.cons(3, 4));
+        System.out.println("3: " + three + "\n");
+        System.out.println("4: " + four + "\n");
     }
 }
 
 class Workload {
-    public void newMethod() {
+    public Pair cons(int f, int l) {
+        return new Pair(f, l);
+    }
+    public int car(Pair p) {
+        return p.first;
+    }
+    public int cdr(Pair p) {
+        return p.last;
+    }
+}
+
+class Pair {
+    int first;
+    int last;
+    public Pair(int f, int l) {
+        this.first = f;
+        this.last = l;
     }
 }
